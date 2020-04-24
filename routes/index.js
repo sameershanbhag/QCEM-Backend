2f151141c0c70900290c5d9427f5925973c4997d
sshanbh1 <sshanbh1@uncc.edu>
2020-04-24 18:05:57 -0400

    Remove Rendering UI from the Service List

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,7 @@ var router = express.Router();
 let sessionStorage = require('../utility/sessionStorage');
 
 router.get('/', (req, res) => {
-  res.render('index', {name: sessionStorage.userName(), title: 'index'});
-});
-
-router.get('/about', (req, res)=>{
-  res.render('about', {title: 'about', name: sessionStorage.userName()});
-});
-
-router.get('/contact', (req, res) => {
-  res.render('contact', {title: 'contact', name: sessionStorage.userName()});
-});
-
-router.post('/contact', (req, res) => {
-  res.render('contact', {title: 'contact', data: "We will get back to you shortly.", name: sessionStorage.userName()});
+  res.status(200).send({"Error" : "Please Use the Front End to navigate through this API"});
 });
 
 module.exports = router;
